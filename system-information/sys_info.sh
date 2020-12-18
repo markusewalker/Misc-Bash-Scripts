4e16e010bd8c1fbcb1224be7a8dbe900b541492a
Markus Walker <52591819+markusewalker@users.noreply.github.com>
2020-12-17 20:07:37 -0800

    Update sys_info.sh

--- system-information/sys_info.sh
+++ system-information/sys_info.sh
@@ -2,7 +2,7 @@
 
 # Authored by   : Markus Walker
 # Original Date : 7/5/19
-# Date Modified : 7/5/19
+# Date Modified : 12/17/20
 
 # Description   : To display the current Linux distro following system information: hostname, disk space,
 #                 memory, uptime and active users.
@@ -42,72 +42,75 @@ displayUsers() {
     echo ${BLANK_SPACE}
 }
 
-echo -e "\e[96m================================================\n"
-echo -e "\tDisplay System Information\n"
-echo -e "================================================\e[0m\n"          
-echo "This script displays information about the current system."
-echo -e "---------------------------------------------------------"
+Main() {
+    echo -e "\x1B[96m================================================\n"
+    echo -e "\tDisplay System Information\n"
+    echo -e "================================================\x1B[0m\n"          
+    echo "This script displays information about the current system."
+    echo -e "---------------------------------------------------------"
 
-# List options available to the user.
-echo -e "\nSee the below options for displaying information about the system."
-echo -e "(1):\t Display Hostname"
-echo -e "(2):\t Display Disk Space"
-echo -e "(3):\t Display Memory"
-echo -e "(4):\t Display Uptime"
-echo -e "(5):\t Display Active Users"
-echo -e "(6):\t Display all options\n"
+    # List options available to the user.
+    echo -e "\nSee the below options for displaying information about the system."
+    echo -e "(1):\t Display Hostname"
+    echo -e "(2):\t Display Disk Space"
+    echo -e "(3):\t Display Memory"
+    echo -e "(4):\t Display Uptime"
+    echo -e "(5):\t Display Active Users"
+    echo -e "(6):\t Display all options\n"
 
-FOREGROUND_COLOR="\e[97m"
-BACKGROUND_COLOR="\e[104m"
-DEFAULT_COLOR="\e[0m"
-BLANK_SPACE=""
+    FOREGROUND_COLOR="\x1B[97m"
+    BACKGROUND_COLOR="\x1B[104m"
+    DEFAULT_COLOR="\x1B[0m"
+    BLANK_SPACE=""
 
-INPUT="yes"
-while [[ ${INPUT} = "yes" ]]
-do
-    # Prompt the user to choose an option.
-    read -p "Please enter which option you would like to use: " CHOICE
+    INPUT="yes"
+    while [[ ${INPUT} = "yes" ]]
+    do
+        # Prompt the user to choose an option.
+        read -p "Please enter which option you would like to use: " CHOICE
 
-    case ${CHOICE} in 
-        1)
-            displayHostName
-            echo ${BLANK_SPACE};;
-        2)
-            displayDiskSpace
-            echo ${BLANK_SPACE};;
-        3)
-            displayMemory
-            echo ${BLANK_SPACE};;
-        4)
-            displayUpTime
-            echo ${BLANK_SPACE};;
-        5)
-            displayUsers
-            echo ${BLANK_SPACE};;
-        6)
-            displayHostName
-            displayDiskSpace
-            displayMemory
-            displayUpTime
-            displayUsers
-            echo ${BLANK_SPACE};;
-        *)
-            echo -e "You must enter a number between 1-6 ONLY.\n";;
-    esac
+        case ${CHOICE} in 
+            1)
+                displayHostName
+                echo ${BLANK_SPACE};;
+            2)
+                displayDiskSpace
+                echo ${BLANK_SPACE};;
+            3)
+                displayMemory
+                echo ${BLANK_SPACE};;
+            4)
+                displayUpTime
+                echo ${BLANK_SPACE};;
+            5)
+                displayUsers
+                echo ${BLANK_SPACE};;
+            6)
+                displayHostName
+                displayDiskSpace
+                displayMemory
+                displayUpTime
+                displayUsers
+                echo ${BLANK_SPACE};;
+            *)
+                echo -e "You must enter a number between 1-6 ONLY.\n";;
+        esac
 
-    # Prompt user if they wish to continue running the script or end it.
-    echo "Do you want to continue? Enter 'yes' or 'no'."
-    read INPUT
+        # Prompt user if they wish to continue running the script or end it.
+        read -p "Do you want to continue? Enter 'yes' or 'no': " INPUT
 
-    if [[ ${INPUT} = "no" ]];
-    then
-        echo -e ${BLANK_SPACE}
-        
-    elif [[ ${INPUT} = "yes" ]];
-    then    
-        echo ${BLANK_SPACE}
-        continue
-    fi
-done
+        if [[ ${INPUT} = "no" ]];
+        then
+            echo -e ${BLANK_SPACE}
+
+        elif [[ ${INPUT} = "yes" ]];
+        then    
+            echo ${BLANK_SPACE}
+            continue
+        fi
+    done
+}
+
+Main "$@"
 
 exit 0
