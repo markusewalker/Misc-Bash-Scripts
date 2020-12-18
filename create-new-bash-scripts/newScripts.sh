95f99e50e39a8d9b210c105ede97e5db702dd283
Markus Walker <52591819+markusewalker@users.noreply.github.com>
2020-12-17 19:34:21 -0800

    Update newScripts.sh

--- create-new-bash-scripts/newScripts.sh
+++ create-new-bash-scripts/newScripts.sh
@@ -6,12 +6,11 @@
 
 # Description   : To create new scripts and to make them executable upon creation.
 
-echo -e "\e[96m==========================================\n"
+echo -e "\x1B[96m==========================================\n"
 echo -e "\tCreate New Scripts Tool\n"
-echo -e "==========================================\e[0m\n"
+echo -e "==========================================\x1B[0m\n"
 
-echo "Enter in a new file name"
-read NAME
-touch $NAME.sh
-echo "#!/bin/bash" >> $NAME.sh
+read -p "Enter in a new file name: " NAME
+touch ${NAME}.sh
+echo "#!/bin/bash" >> ${NAME}.sh
 chmod +x $NAME.sh
